b982d713032577e99b096c99313790a52a8b9ca5
wsq <3053878970@qq.com>
2019-09-18 21:47:38 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -9,14 +9,19 @@ module.exports = {
             entry: 'src/pages/index/main.js',
             template: 'public/index.html',
             filename: 'index.html',
+            title: '首页'
         },
         multipage_test: {
             entry: 'src/pages/multipage_test/main.js',
-            template: 'public/index.html'
+            template: 'public/index.html',
+            filename: 'multipage_test.html',
+            title: '多页面测试'
         },
         video: {
             entry: 'src/pages/video/main.js',
-            template: 'public/index.html'
+            template: 'public/index.html',
+            filename: 'video.html',
+            title: '视频监控'
         }
     }
 }
